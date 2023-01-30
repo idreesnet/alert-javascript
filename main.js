9b01d43d376f27273dc9f80e66f547893fd2ba55
idreesnet <54152894+idreesnet@users.noreply.github.com>
2023-01-30 23:59:00 +0500

    alert practice file added
    a js file containing alert method's comprehensive code with an index.html and a pdf file added.

---
+++ main.js
@@ -0,0 +1,17 @@
+//Q1
+// alert("Assalam-o-Alaikum");
+
+//Q2
+// alert("Error! Please enter a valid password.");
+
+
+//Q3
+// alert("Welcome to JS Land.. \nHappy Coding!");
+
+//Q4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding \nPrevent this page from creating additional dialogs.");
+
+
+//Q5 
+alert("Hello... I can run JS through my web browser's console");
